c4c284c7135e77ade54fbf501fe5c4559ccf7b82
Niklp09 <ngs.nik@outlook.de>
2022-07-09 17:14:14 +0200

    use player:get_meta() instead of player:get/set_attribute()

--- init.lua
+++ init.lua
@@ -152,19 +152,19 @@ minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
 
 	-- If database item exists and new storage item does not, use database item
-	if player:get_attribute("ranks:rank") ~= nil and storage:get_string(name, rank) == "" then
+	if player:get_meta():get("ranks:rank") ~= nil and storage:get_string(name, rank) == "" then
 		-- Add entry into new storage system
-		storage:set_string(name, player:get_attribute("ranks:rank"))
+		storage:set_string(name, player:get_meta():get("ranks:rank"))
 
 		-- Store backup then invalidate database item
-		player:set_attribute("ranks:rank-old", player:get_attribute("ranks:rank"))
-		player:set_attribute("ranks:rank", nil)
+		player:get_meta():set("ranks:rank-old", player:get_meta():get("ranks:rank"))
+		player:get_meta():set("ranks:rank", nil)
 	end
 
 	-- Both items exist, remove old one
-	if player:get_attribute("ranks:rank") ~= nil and storage:get_string(name, rank) ~= "" then
-		player:set_attribute("ranks:rank-old", player:get_attribute("ranks:rank"))
-		player:set_attribute("ranks:rank", nil)
+	if player:get_meta():get("ranks:rank") ~= nil and storage:get_string(name, rank) ~= "" then
+		player:get_meta():set("ranks:rank-old", player:get_meta():get("ranks:rank"))
+		player:get_meta():set("ranks:rank", nil)
 	end
 
 	if ranks.get_rank(name) then
